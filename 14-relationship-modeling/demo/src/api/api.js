2c4e3229fc8b784e6dafc0f24579e24339b8c0c5
DukeOfEtiquette <duquetteadam@gmail.com>
2018-09-24 23:32:27 -0700

    demo 14 prep

---
+++ 14-relationship-modeling/demo/src/api/api.js
@@ -0,0 +1,42 @@
+'use strict';
+
+import express from 'express';
+const router = express.Router();
+
+// Dynamic Models
+// This will use a model matching /:model/ in all routes that have a model parameter
+import modelFinder from '../middleware/models.js';
+router.param('model', modelFinder);
+
+// Each of our REST endpoints simply calls the model's appropriate CRUD Method (only give the students GET and POST for now)
+// In all cases, we just catch(next), which feeds any errors we get into the next() as a param
+// This fires off the error middleware automatically.  Otherwise, we send out a formatted JSON Response
+
+router.get('/api/v1/:model', (req,res,next) => {
+  req.model.find({})
+    .then( data => sendJSON(res,data) )
+    .catch( next );
+});
+
+router.get('/api/v1/:model/:id', (req,res,next) => {
+  req.model.findOne({_id:req.params.id})
+    .then( data => sendJSON(res,data) )
+    .catch( next );
+});
+
+router.post('/api/v1/:model', (req,res,next) => {
+  let record = new req.model(req.body);
+  record.save()
+    .then( data => sendJSON(res,data) )
+    .catch( next );
+});
+
+let sendJSON = (res,data) => {
+  res.statusCode = 200;
+  res.statusMessage = 'OK';
+  res.setHeader('Content-Type', 'application/json');
+  res.write( JSON.stringify(data) );
+  res.end();
+};
+
+export default router;
---
+++ 14-relationship-modeling/demo/src/models/pokemons.js
@@ -0,0 +1,80 @@
+'use strict'; // +60
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let types = [
+  'bug',
+  'dark',
+  'dragon',
+  'electric',
+  'fairy',
+  'fighting',
+  'fire',
+  'flying',
+  'ghost',
+  'grass',
+  'ground',
+  'ice',
+  'bormal',
+  'poison',
+  'psychic',
+  'rock',
+  'steel',
+  'water',
+  'none'
+];
